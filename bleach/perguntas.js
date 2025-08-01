ae9bd06e946e9bca3a1550305d201b7c975b2538
EduFelipeGGEZ <eduardo.pedra@escola.pr.gov.br>
2025-08-01 09:48:27 -0300

    Add files via upload

---
+++ bleach/perguntas.js
@@ -0,0 +1,17 @@
+Criarcartao(
+   'programação',
+   'o que é javascript',
+   'o javascript é uma linguagem de programação'
+)
+
+Criarcartao(
+     'Geografia',
+     'qual é a capital do paraná?',
+    'a capital do paraná é curitiba'
+)
+
+Criarcartao(
+        'programação',
+        'o que é python?',
+        'pyton é uma linguagem de programação'
+)
